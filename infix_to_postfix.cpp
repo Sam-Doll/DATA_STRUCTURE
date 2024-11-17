820f94a2ffde4433265fb64b6dd93892df9ef1d8
Smriti Gupta <135643404+Sam-Doll@users.noreply.github.com>
2024-11-18 01:41:46 +0530

    added infix_to_postfix.cpp

---
+++ infix_to_postfix.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int precedence(char c) {
+    if (c == '+' || c == '-') return 1;
+    if (c == '*' || c == '/') return 2;
+    return 0;
+}
+
+string infixToPostfix(string infix) {
+    stack<char> st;
+    string result;
+
+    for (char c : infix) {
+        if (isalnum(c)) {
+            result += c;
+        } else if (c == '(') {
+            st.push(c);
+        } else if (c == ')') {
+            while (!st.empty() && st.top() != '(') {
+                result += st.top();
+                st.pop();
+            }
+            st.pop();
+        } else {
+            while (!st.empty() && precedence(st.top()) >= precedence(c)) {
+                result += st.top();
+                st.pop();
+            }
+            st.push(c);
+        }
+    }
+
+    while (!st.empty()) {
+        result += st.top();
+        st.pop();
+    }
+
+    return result;
+}
+
+int main(){
+    string infix;
+    cout<<"enter expression: "<< endl;
+    cin >> infix;
+    cout << infixToPostfix(infix) << endl;
+    return 0;
+}
